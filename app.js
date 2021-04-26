128c8db7deed650929dc9151c9744987f6ee0182
ibrahem <ibrahemhassan35@gmail.com>
2021-04-26 20:41:36 +0200

    3rd

--- app.js
+++ app.js
@@ -257,16 +257,5 @@ app.delete('/api/students/:id', (req, res) => {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
 const port = process.env.PORT || 3000;
 app.listen(port,()=>console.log(`listening on port ${port}...`) );
